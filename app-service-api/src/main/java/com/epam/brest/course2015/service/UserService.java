6400bec3c5f2ab918be38f786545689df5fdabf5
Arideverchi <sasha-karpovich@mail.ru>
2015-10-16 22:54:14 +0300

    added app-service

--- app-service-api/src/main/java/com/epam/brest/course2015/service/UserService.java
+++ app-service-api/src/main/java/com/epam/brest/course2015/service/UserService.java
@@ -4,13 +4,18 @@ import com.epam.brest.course2015.domain.User;
 
 import java.util.List;
 
-/**
- * Created by juga on 14.10.15.
- */
 public interface UserService {
 
     public List<User> getAllUsers();
 
+    public User getUserById(Integer userId);
+
+    public User getUserByLogin(String login);
+
     public Integer addUser(User user);
+
+    public void updateUser(User user);
+
+    public void deleteUser(Integer userId);
 
 }
