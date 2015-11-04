40cc01ce74c86ef69199d63fa79d2420bf5051c1
Arideverchi <sasha-karpovich@mail.ru>
2015-11-04 16:34:21 +0300

    sample moved to directory

--- sample/app-web/src/main/java/com/epam/brest/course2015/web/UserController.java
+++ sample/app-web/src/main/java/com/epam/brest/course2015/web/UserController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-
+/**
+ * Created by juga on 30.10.15.
+ */
 @Controller
 public class UserController {
 
