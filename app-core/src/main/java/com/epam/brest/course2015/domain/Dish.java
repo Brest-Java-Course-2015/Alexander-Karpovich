ab4f048a54a17608ff7e12ec99f04a9318ea5b3d
Arideverchi <sasha-karpovich@mail.ru>
2015-11-04 17:57:34 +0300

    addede app-core and Dish class

---
+++ app-core/src/main/java/com/epam/brest/course2015/domain/Dish.java
@@ -0,0 +1,38 @@
+package com.epam.brest.course2015.domain;
+
+/**
+ * Created by ntrln on 04.11.15.
+ */
+public class Dish {
+	private Integer dishId;
+
+	private String name;
+
+	private Integer cost;
+
+	public Integer getDishId() {
+		return dishId;
+	}
+
+	public void setDishId(Integer dishId) {
+		this.dishId = dishId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCost() {
+		return cost;
+	}
+
+	public void setCost(Integer cost) {
+		this.cost = cost;
+	}
+
+
+}
