b1a5658b27ada78d4ea769b4e4afd879b8ca2f1f
Arideverchi <sasha-karpovich@mail.ru>
2015-11-06 02:04:04 +0300

    dto

--- app-core/src/main/java/com/epam/brest/course2015/dto/DishDto.java
+++ app-core/src/main/java/com/epam/brest/course2015/dto/DishDto.java
@@ -1,7 +1,29 @@
 package com.epam.brest.course2015.dto;
 
+import com.epam.brest.course2015.domain.Dish;
+
+import java.util.List;
+
 /**
  * Created by ntrln on 04.11.15.
  */
 public class DishDto {
+	private List<Dish> dishes;
+	private Integer total;
+
+	public List<Dish> getDishes() {
+		return dishes;
+	}
+
+	public void setDishes(List<Dish> dishes) {
+		this.dishes = dishes;
+	}
+
+	public Integer getTotal() {
+		return total;
+	}
+
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
 }
